431eea9518ffdcda3dc747ac5514cf9e6da94544
tamanugi <tamanugi@gmail.com>
2019-09-06 18:55:35 +0900

    👍 とりあえず10:00カウントダウンできるように

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,43 @@
-<template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
-  </div>
+<template lang="pug">
+  #app(@click="toggleTimerActive")
+    .timer-wrapper
+      .timer {{ countDownTime }}
 </template>
 
-<style>
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component
+export default class extends Vue {
+  private restTime = 10 * 60
+  public timerId: number | null = null
+
+  public get countDownTime(): string {
+    const minutes = Math.floor(this.restTime / 60)
+    const seconds = this.restTime % 60
+    return `${this.zeroPadding(minutes)}:${this.zeroPadding(seconds)}`
+  }
+
+  public toggleTimerActive() {
+    if (!this.timerId) {
+      this.timerId = setInterval(() => {
+        if (this.restTime > 0) {
+          this.restTime-- 
+        }
+      }, 1000)
+    } else { 
+      clearInterval(this.timerId)
+      this.timerId = null
+    }
+  }
+
+  private zeroPadding(s: number): string {
+    return ('00' + s).slice(-2)
+  }
+}
+</script>
+
+<style lang="stylus">
 #app {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -16,16 +45,18 @@
   text-align: center;
   color: #2c3e50;
 }
-#nav {
-  padding: 30px;
-}
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+body
+  background-color #707070
 
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+.timer-wrapper
+  display flex
+  justify-content center
+  align-items center
+  height calc(100vh - 16px)
+  width 100%
+
+  color #ffffff
+  font-size 512px
+
 </style>
